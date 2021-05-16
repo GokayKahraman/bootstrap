c943a7f33a8021f242477e77c5fa95d04236372c
gokay <gokay.kahraman@yandex.com>
2021-05-16 20:51:26 +0300

    menüjs

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,235 +1,59 @@
 <template>
   <div class="container">
-    <ul class="nav">
+    <h4>Tab Menü</h4>
 
-      <li class="nav-item">
-        <a href="#" class="nav-link">deneme</a>
-      </li>
-
-      <li class="nav-item">
-        <a href="#" class="nav-link active">deneme</a>
-      </li>
-
-      <li class="nav-item">
-        <a href="#" class="nav-link">deneme</a>
-      </li>
-
-      <li class="nav-item">
-        <a href="#" class="nav-link disabled">deneme</a>
-      </li>
-    </ul>
-
-    <hr>
-
-    <h4>Basit Yatay Menü - Ortalama</h4>
-
-    
-    <ul class="nav justify-content-center">
-
-      <li class="nav-item">
-        <a href="#" class="nav-link">deneme</a>
-      </li>
-
-      <li class="nav-item">
-        <a href="#" class="nav-link active">deneme</a>
-      </li>
-
-      <li class="nav-item">
-        <a href="#" class="nav-link">deneme</a>
-      </li>
-
-      <li class="nav-item">
-        <a href="#" class="nav-link disabled">deneme </a>
-      </li>
-    </ul>
-
-    <hr>
-
-    <h4>Basit Yatay Menü - Sağa Yaslama</h4>
-
-    
-    <ul class="nav justify-content-end">
-
-      <li class="nav-item">
-        <a href="#" class="nav-link">deneme</a>
-      </li>
-
-      <li class="nav-item">
-        <a href="#" class="nav-link active">deneme</a>
-      </li>
-
-      <li class="nav-item">
-        <a href="#" class="nav-link">deneme</a>
-      </li>
-
-      <li class="nav-item">
-        <a href="#" class="nav-link disabled">deneme</a>
-      </li>
-    </ul>
-
-    <hr>
-
-    <h4>Basit Dikey Menü </h4>
-
-    
-    <ul class="nav flex-column">
-
-      <li class="nav-item">
-        <a href="#" class="nav-link">deneme</a>
-      </li>
-
-      <li class="nav-item">
-        <a href="#" class="nav-link active">deneme</a>
-      </li>
-
-      <li class="nav-item">
-        <a href="#" class="nav-link">deneme</a>
-      </li>
-
-      <li class="nav-item">
-        <a href="#" class="nav-link disabled">deneme</a>
-      </li>
-    </ul>
-
-
-    <hr>
-
-    <h4>Tab Menü </h4>
-    <br>
-
-    
     <ul class="nav nav-tabs">
-
+      
       <li class="nav-item">
-        <a href="#" class="nav-link active">deneme</a>
+        <a href="#udemy" data-toggle="tab" class="nav-link">Udemy Kursu</a>
       </li>
 
       <li class="nav-item">
-        <a href="#" class="nav-link ">deneme</a>
-      </li>
+       <a href="#youtube" data-toggle="tab" class="nav-link">Youtube Kanalım</a>
+     </li>
 
-      <li class="nav-item">
-        <a href="#" class="nav-link">deneme</a>
-      </li>
+     <li class="nav-item">
+       <a href="#deneme" data-toggle="tab" class="nav-link">deneme Sayfam</a>
+     </li>
 
-      <li class="nav-item">
-        <a href="#" class="nav-link disabled">deneme</a>
-      </li>
-    </ul>
+   </ul>
+   <br>
 
-    <hr>
-
-    <h4>Hap Pills Menü </h4>
+    <div class="tab-content">
+      <div class="tab-pane fade active" id="udemy">Udemy Sekmesi -Lorem ipsum dolor </div>
+      <div class="tab-pane fade" id="youtube">Youtube Sekmesi -Lorem ipsum dolor sit amet</div>
+      <div class="tab-pane fade" id="deneme">Youtube Sekmesi -Lorem ipsum dolor sit ametr </div>
+    </div>
     <br>
-
-    
-    <ul class="nav nav-pills">
-
-      <li class="nav-item">
-        <a href="#" class="nav-link active">deneme</a>
-      </li>
-
-      <li class="nav-item">
-        <a href="#" class="nav-link ">deneme</a>
-      </li>
-
-      <li class="nav-item">
-        <a href="#" class="nav-link active">deneme</a>
-      </li>
-
-      <li class="nav-item">
-        <a href="#" class="nav-link disabled">Ydeneme</a>
-      </li>
-    </ul>
-
-    <hr>
-
-    <h4>Hap Pills Menü Sayfaya Yayma</h4>
     <br>
+    <div class="row">
+  <ul class="nav flex-column nav-pills">
 
-    
-    <ul class="nav nav-pills nav-fill">
+    <li class="nav-item">
+      <a href="#udemy" data-toggle="tab" class="nav-link">Udemy Kursu</a>
 
-      <li class="nav-item">
-        <a href="#" class="nav-link active">deneme</a>
-      </li>
+    </li>
 
-      <li class="nav-item">
-        <a href="#" class="nav-link ">deneme</a>
-      </li>
+    <li class="nav-item">
+     <a href="#youtube" data-toggle="tab" class="nav-link">Youtube Kanalım</a>
 
-      <li class="nav-item">
-        <a href="#" class="nav-link active">deneme</a>
-      </li>
+   </li>
 
-      <li class="nav-item">
-        <a href="#" class="nav-link disabled">deneme</a>
-      </li>
+   <li class="nav-item">
+     <a href="#emrah" data-toggle="tab" class="nav-link"> Sayfam</a>
 
-      <li class="nav-item">
-        <a href="#" class="nav-link disabled">deneme</a>
-      </li>
-    </ul>
+   </li>
 
-    <hr>
+ </ul>
 
-    <h4>Eşit Dağılma</h4>
-    <br>
+    <div class="tab-content col-md-9">
+      <div class="tab-pane fade active" id="udemy">Udemy Sekmesi jrrtjjrtjtetjhetjh</div>
+      <div class="tab-pane fade" id="youtube">Youtube Sekmesi gsdgsdgsd</div>
 
-    
-    <ul class="nav nav-pills nav-justified">
-
-      <li class="nav-item">
-        <a href="#" class="nav-link active">deneme</a>
-      </li>
-
-      <li class="nav-item">
-        <a href="#" class="nav-link ">deneme</a>
-      </li>
-
-      <li class="nav-item">
-        <a href="#" class="nav-link active">deneme</a>
-      </li>
-
-      <li class="nav-item">
-        <a href="#" class="nav-link disabled">deneme</a>
-      </li>
-
-      <li class="nav-item">
-        <a href="#" class="nav-link disabled">deneme</a>
-      </li>
-    </ul>
-
-    <hr>
-
-
-     <h4>Esnek Yapı</h4>
-    <br>
-
-    
-    <ul class="nav nav-pills nav-justified flex-column flex-sm-row">
-
-      <li class="nav-item">
-        <a href="#" class="nav-link active">Udemy</a>
-      </li>
-
-      <li class="nav-item">
-        <a href="#" class="nav-link ">deneme</a>
-      </li>
-
-      <li class="nav-item">
-        <a href="#" class="nav-link active">GÖkay kahraman</a>
-      </li>
-
-      <li class="nav-item">
-        <a href="#" class="nav-link disabled">deneme</a>
-      </li>
-
-      <li class="nav-item">
-        <a href="#" class="nav-link disabled">deneme</a>
-      </li>
-    </ul>
+      <div class="tab-pane fade" id="emrah">Youtube Sekmesi gfgfsdgsdfgsfd.</div>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
