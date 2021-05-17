1c2b883b551390121eaa912a05f53dd55c15504f
gokay <gokay.kahraman@yandex.com>
2021-05-17 14:59:23 +0300

    modal

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,106 +1,72 @@
 <template>
   <div class="container">
-    <h1>İlerleme Çubuğu (Progress)</h1>
+    <h1>Açılır Kutu (Modal)</h1>
 		<hr>
 
-    <div class="progress">
-      <div class="progress-bar" style="width: 25%">%25</div>
-    </div>
 
-    <br>
+    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#modal1">Modal Aç</button>
 
-    <div class="progress">
-      <div class="progress-bar" style="width: 50%">%50</div>
-    </div>
+    <div class="modal fade" id="modal1">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Udemy Bootstrap Kursu</h5>
+            <button type="button" class="close" data-dismiss="modal">
+              <span>&times;</span>
+            </button>
+          </div>
 
-     <br>
+          <div class="modal-body">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </div>
 
-     <div class="progress">
-      <div class="progress-bar" style="width: 75%">%75</div>
-    </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-success">Kaydet</button>
+            <button type="button" class="btn btn-danger" data-dismiss="modal" >Çık</button>
 
-     <br>
-
-    <div class="progress">
-      <div class="progress-bar" style="width: 100%">%100</div>
+          </div>
+        </div>
+      </div>
     </div>
 
     <hr>
 
-    <div class="progress">
-      <div class="progress-bar bg-success" style="width: 25%">%25</div>
+    <h4>Form Özellikleri</h4>
+
+    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#modal2">Modal Aç</button>
+
+    <div class="modal fade bd-example-modal-sm" id="modal2">
+      <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Udemy Bootstrap Kursu</h5>
+            <button type="button" class="close" data-dismiss="modal">
+              <span>&times;</span>
+            </button>
+          </div>
+
+          <div class="modal-body">
+         <div class="form-group">
+           <label class="form-control-label">Adınız</label>
+           <input type="text" class="form-control" placeholder="Adınız" name="">
+         </div>
+         <div class="form-group">
+           <label class="form-control-label">Soyadınız</label>
+           <input type="text" class="form-control" placeholder="Soyadınız" name="">
+         </div>
+          </div>
+
+          <div class="modal-footer">
+            <button type="button" class="btn btn-success">Kaydet</button>
+            <button type="button" class="btn btn-danger" data-dismiss="modal" >Çık</button>
+
+          </div>
+        </div>
+      </div>
     </div>
-
-    <br>
-
-    <div class="progress">
-      <div class="progress-bar bg-danger" style="width: 50%">%50</div>
-    </div>
-
-     <br>
-
-     <div class="progress">
-      <div class="progress-bar bg-info" style="width: 75%">%75</div>
-    </div>
-
-     <br>
-
-    <div class="progress">
-      <div class="progress-bar bg-dark" style="width: 100%">%100</div>
-    </div>
-
-    <h4>Striped</h4>
-
-     <hr>
-
-    <div class="progress">
-      <div class="progress-bar bg-success progress-bar-striped" style="width: 25%">%25</div>
-    </div>
-
-    <br>
-
-    <div class="progress">
-      <div class="progress-bar bg-danger progress-bar-striped" style="width: 50%">%50</div>
-    </div>
-
-     <br>
-
-     <div class="progress">
-      <div class="progress-bar bg-info progress-bar-striped" style="width: 75%">%75</div>
-    </div>
-
-     <br>
-
-    <div class="progress">
-      <div class="progress-bar bg-dark progress-bar-striped " style="width: 100%; height: 40px;">%100</div>
-    </div>
-
-     <h4>Animated</h4>
-
-     <hr>
-
-    <div class="progress">
-      <div class="progress-bar bg-success progress-bar-striped progress-bar-animated" style="width: 25%">%25</div>
-    </div>
-
-    <br>
-
-    <div class="progress">
-      <div class="progress-bar bg-danger progress-bar-striped progress-bar-animated" style="width: 50%">%50</div>
-    </div>
-
-     <br>
-
-     <div class="progress">
-      <div class="progress-bar bg-info progress-bar-striped progress-bar-animated" style="width: 75%">%75</div>
-    </div>
-
-     <br>
-
-    <div class="progress">
-      <div class="progress-bar bg-dark progress-bar-striped progress-bar-animated" style="width: 100%">%100</div>
-    </div>
-
+    
   </div>
 </template>
 
